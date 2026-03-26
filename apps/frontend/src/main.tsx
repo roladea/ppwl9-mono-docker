7cfb0070eb17d4778afe83243a28f7c899b735ef
roladea <h1101241016@student.untan.ac.id>
2026-03-27 00:48:28 +0700

    monorepo3

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.tsx'
-// import App from './ApiTest'
-import App from './App2'
+import App3 from './App3.tsx' 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App3 /> {}
   </StrictMode>,
 )
